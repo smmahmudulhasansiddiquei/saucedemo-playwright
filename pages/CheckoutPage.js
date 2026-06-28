319b18cdd8a8d1dbc633b7c7873321dd285fb725
smmahmudulhasansiddiquei <mahmudcse03@gmail.com>
2026-06-29 01:50:44 +0600

    checkout page done

--- pages/CheckoutPage.js
+++ pages/CheckoutPage.js
@@ -15,6 +15,39 @@ export class CheckoutPage extends BasePage {
 
     }
 
+    async enterInformation(first, last, zip) {
+
+        await this.fill(this.firstName, first);
+        await this.fill(this.lastName, last);
+        await this.fill(this.postalCode, zip);
+
+    }
+
+    async continueCheckout() {
+
+        await this.click(this.continueBtn);
+
+    }
+
+    async finishOrder() {
+
+        await this.click(this.finishBtn);
+
+    }
+
+    async completeCheckout(first, last, zip) {
+
+        await this.enterInformation(first, last, zip);
+        await this.continueCheckout();
+        await this.finishOrder();
+
+    }
+
+    async successMessage() {
+
+        return await this.getText(this.completeHeader);
+
+    }
    
 
 }
